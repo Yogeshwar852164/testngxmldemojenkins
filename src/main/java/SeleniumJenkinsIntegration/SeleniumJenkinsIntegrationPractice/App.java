1fd7c6bb4fbfe7d526fc5467894e0ba741078928
Pichewar <852164@cognizant.com>
2023-04-04 22:09:10 -0700

    my first commit

---
+++ src/main/java/SeleniumJenkinsIntegration/SeleniumJenkinsIntegrationPractice/App.java
@@ -0,0 +1,13 @@
+package SeleniumJenkinsIntegration.SeleniumJenkinsIntegrationPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
